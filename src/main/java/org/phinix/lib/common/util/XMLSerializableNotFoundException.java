85bf56912783495bae6e4c033c13fb5f7a9b410a
lPhiNix <pablomargamer05@gmail.com>
2024-11-25 13:11:25 +0100

    Added XMLManager to create XML from generic class type list and XMLSerializableModel annotacion to mark classes that are serializable to XML

--- src/main/java/org/phinix/lib/common/util/XMLSerializableNotFoundException.java
+++ src/main/java/org/phinix/lib/common/util/XMLSerializableNotFoundException.java
@@ -1,3 +1,21 @@
 package org.phinix.lib.common.util;
 
+/**
+ * <p>Exception thrown when an object that is not annotated with {@link XMLSerializableModel}
+ * is attempted to be serialized to XML.
+ * This exception is used to indicate that the class of the object does not meet the
+ * required criteria for XML serialization, which is the presence of the
+ * {@link XMLSerializableModel} annotation.</p>
+ *
+ * Example use case:
+ *
+ * <pre>
+ * if (!object.getClass().isAnnotationPresent(XMLSerializableModel.class)) {
+ *     throw new XMLSerializableNotFoundException("Class is not serializable to XML.");
+ * }
+ * </pre>
+ *
+ * This exception ensures that only objects marked as serializable are processed by
+ * the XMLManager class, preventing errors during the XML generation process.
+ */
 public class XMLSerializableNotFoundException extends Throwable {}
